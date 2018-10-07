c5ffef6f8dbb513c7bf792ec5a5992592cad9f5f
Stephen <schow94@gmail.com>
2018-10-07 17:18:33 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+var bodyParser = require("body-parser"), 
+    //mongoose   = require("mongoose"),
+    express    = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser");
+    
+    
+//mongoose.connect("mongodb://localhost:27017/personalwebsite", {newUrlParser:true});
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+//INDEX
+app.get("/", function(req, res){
+   res.render("index");
+});
+
+//EDUCATION
+app.get("/education", function(req, res){
+    res.render("education");
+});
+//WORK
+app.get("/work", function(req, res){
+    res.render("work");
+});
+
+// PROJECTS
+app.get("/projects", function(req, res){
+    res.render("projects");
+});
+
+//PHOTOGRAPHY
+app.get("/photography", function(req, res){
+    res.render("photography");
+});
+
+
+//CONTACT ME
+app.get("/contactme", function(req, res){
+    res.render("contactme");
+});
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("SERVER IS RUNNING!!!");
+});
+
